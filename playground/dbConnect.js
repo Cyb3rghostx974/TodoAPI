0665c387052d226c494ff1bb979397d8a261f839
Cyb3rghostx974 <ludovic.lvnr@gmail.com>
2018-10-08 12:58:04 +0400

    Database connected

---
+++ playground/dbConnect.js
@@ -0,0 +1,53 @@
+//const mongoClient = require('mongodb').MongoClient; // La librairie npm MongoDB
+
+const {MongoClient} = require('mongodb');
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, client) => {
+    
+    if(err)
+    {
+        
+        return console.log(err);
+        
+    }
+    
+    console.log("Connecté !");
+    const db = client.db('TodoApp');
+    
+    db.collection('Users').insertOne({
+        
+        id: '1',
+        email: 'ludovic.lvnr@gmail.com',
+        password: 'blablasecret',
+        completed: true
+        
+    }, (err, result) => {
+        
+       if(err)
+        {
+            
+            return console.log('unable to insert todo', err);
+            
+        }
+        
+        console.log(JSON.stringify(result.ops));
+        
+    });
+    
+    
+    client.close();
+    
+});
+
+/*var User = {
+    
+  name: "Bob"  
+    
+};
+
+
+console.log(User.name);
+
+var {name} = User;
+
+console.log(name);*/
